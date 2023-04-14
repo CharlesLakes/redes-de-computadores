d296b7e8cd8b18a85f70ac0ec4d1c6632546f579
Carlos Lagos <carlos.lagosc@usm.cl>
2023-04-13 21:52:17 -0400

    comentarios

--- Tarea1/main.py
+++ Tarea1/main.py
@@ -1,11 +1,19 @@
 import socket
 
+# Nombre: get_data_image
+# Parametros: s: socket.socket, host: str, port: int
+# Descripción: Envia un mensaje al servidor para obtener los datos de la imagen
+
 def get_data_image(s: socket.socket,host,port):
     s.sendto("GET NEW IMG DATA".encode(),(host,port))
     respuesta = s.recvfrom(1024)[0].decode()
     print("Mensaje Recibido: " + respuesta)
     return respuesta
-    
+
+# Nombre: data_to_dict
+# Parametros: data: str
+# Descripción: Convierte los datos de la imagen en un diccionario para facilitar su manejo
+
 def data_to_dict(data):
     d = {}
     data = data.strip().split(" ")
@@ -17,6 +25,10 @@ def data_to_dict(data):
             c += 1;
     d["PARTS"] = c
     return d
+
+# Nombre: get_p1tcp
+# Parametros: host: str, port: int, data: dict
+# Descripción: Envia un mensaje al servidor para obtener la primera parte de la imagen haciendo uso de TCP
 
 def get_p1tcp(host,port,data):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -33,6 +45,10 @@ def get_p1tcp(host,port,data):
     print("Mensaje recibido de: <jdiaz.inf.santiago.usm.cl>:<" + str(port) + "> por <TCP>: Bytes recibidos")
     return response
 
+# Nombre: get_pudp
+# Parametros: s: socket.socket, host: str, port: int, data: dict, part: int
+# Descripción: Envia un mensaje al servidor para obtener la segunda o tercera parte de la imagen haciendo uso de UDP
+
 def get_pudp(s,host,port,data,part):
     parts = data["PARTS"]
     ID = data["ID"]
@@ -43,6 +59,10 @@ def get_pudp(s,host,port,data,part):
     print("Mensaje recibido de: <jdiaz.inf.santiago.usm.cl>:<" + str(port) + "> por <UDP>: Bytes recibidos")
 
     return s.recvfrom(int(data["W"])*int(data["H"])*3)[0]
+
+# Nombre: verify_img
+# Parametros: host: str, port: int, data: bytes, data2: dict
+# Descripción: Envia un mensaje al servidor para verificar si la imagen creada es correcta
 
 def verify_img(host,port,data,data2):
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
@@ -64,6 +84,8 @@ def verify_img(host,port,data,data2):
         return 0
         s.close()
 
+# While infinito, parte principal del programa, se rompe al obtener una imagen correcta.
+
 while(1):
     # addres and port
     HOST = "jdiaz.inf.santiago.usm.cl"
@@ -77,8 +99,10 @@ while(1):
     # get image data from host
     data_img = get_data_image(s_udp,HOST,PORT)
 
+    # convert data to dict
     data_img = data_to_dict(data_img)
 
+    # get image parts
     p1tcp = get_p1tcp(HOST,int(data_img["P1TCP"]),data_img)
     p2tcp = get_pudp(s_udp,HOST,int(data_img["P2UDP"]),data_img,2)
     if data_img["PARTS"] > 2:
@@ -86,11 +110,13 @@ while(1):
 
     print("Iniciando escritura de imagen")
 
+    # create image
     img_binary = p1tcp
     img_binary += p2tcp
     if(data_img["PARTS"] > 2):
         img_binary += p3tcp
 
+    # verify image
     condicional = verify_img(HOST, data_img["PV"], img_binary,data_img)
 
     if(condicional == 1):
